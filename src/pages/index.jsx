64ff78213c1827b26258279f4191520e06d0039e
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2021-05-08 22:45:23 -0300

    feat: using bulma classes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,15 +7,19 @@ import Seo from '../components/Seo';
 const Home = () => (
   <Layout>
     <Seo />
-    <h1>Carlos Kvasir</h1>
-    <h2>Backend developer</h2>
-    <h4>Hello, I'm Carlos Lima Junior but in internet aka Carlos Kvasir</h4>
-    <p>I'm a Backend Developer and study about devops.</p>
-    <p>I develop projects mostly with Ruby on Rails.</p>
-    <p>
-      {'Check out mostly '}
-      <Link to="/projects">projects</Link>
-    </p>
+    <header className="has-text-centered">
+      <h1 className="title">Carlos Kvasir</h1>
+      <h2 className="subtitle">Backend developer</h2>
+    </header>
+    <section className="section content is-size-4-desktop is-size-5-touch">
+      <h4 className="title">Hello, I'm Carlos Lima Junior but in internet aka Carlos Kvasir</h4>
+      <p>I'm a Backend Developer and study about devops.</p>
+      <p>I develop projects mostly with Ruby on Rails.</p>
+      <p>
+        {'Check out mostly '}
+        <Link to="/projects">projects</Link>
+      </p>
+    </section>
   </Layout>
 );
 
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -6,15 +6,25 @@ import Layout from '../components/Layout';
 const Projects = () => (
   <Layout>
     <Seo pageTitle="Projects" />
-    <h1>Projects</h1>
-    <hr />
-    <p>
-      {'Some of my projects, see more on my '}
-      <a href="http://github.com/carloskvasir">GitHub</a>
-    </p>
-    <ul>
-      <a href="https://github.com/carloskvasir/terraform-in-action">Terraform</a>
-    </ul>
+    <section className="section is-size-4-desktop is-size-5-touch">
+      <h1 className="title has-text-centered">Projects</h1>
+      <hr />
+      <p className="has-text-centered">
+        {'Some of my projects, see more on my '}
+        <a href="http://github.com/carloskvasir">GitHub</a>
+      </p>
+      <ul className="has-text-centered">
+        <li>
+          <a href="https://github.com/carloskvasir/terraform-in-action">Terraform</a>
+        </li>
+        <li>
+          <a href="https://github.com/carloskvasir/terraform-in-action">Terraform 1</a>
+        </li>
+        <li>
+          <a href="https://github.com/carloskvasir/terraform-in-action">Terraform 2</a>
+        </li>
+      </ul>
+    </section>
   </Layout>
 );
 
