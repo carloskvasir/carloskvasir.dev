333299850fa85bdf1a03cfbf8bb2a7905004016b
Carlos Kvasir <git@carloskvasir.dev>
2025-02-07 06:26:22 -0300

    feat: update projects and translate to Portuguese
    - add images in fake projects
    - Replaced projects in the `project.json` file with new fictional projects
    - Translated the page name from "Projects" to "Projetos"
    - Updated descriptive texts and keywords in the `Seo` component
    - Refactored the project rendering function to `projectRender`
    - Adjusted the navbar link to reflect the translation "Projects" → "Projetos"

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -6,9 +6,9 @@ import Layout from '@components/Layout';
 
 export const Head = () => (
   <Seo
-    pageTitle="Projects"
-    description="Back-end developer, developing apps and apis"
-    keywords="Ruby, Rails, API, developer"
+    pageTitle="Projetos"
+    description="Desenvolvedor back-end, criando aplicativos e APIs"
+    keywords="Ruby, Rails, API, desenvolvedor"
   />
 );
 
@@ -32,38 +32,54 @@ const Projects = () => {
 
   const projects = data.allProjectJson.edges.map(edge => edge.node);
 
+  function projectRender(projeto) {
+    // Gerar um ID único baseado no nome do projeto para obter imagens diferentes
+    const imageId = Math.abs(
+      projeto.name.split("").reduce((acc, char) => acc + char.charCodeAt(0), 0) % 100
+    );
+
+    return (
+      <div key={projeto.id} className="column is-one-third">
+        <div className="card">
+          <div className="card-image">
+            <figure className="image is-4by3">
+              <img
+                src={`https://picsum.photos/seed/${imageId}/400/300`}
+                alt={`Imagem ilustrativa do projeto ${projeto.name}`}
+              />
+            </figure>
+          </div>
+          <div className="card-content">
+            <h3 className="title is-4">{projeto.name}</h3>
+            <p className="subtitle is-6">{projeto.description}</p>
+            <div className="tags">
+              {projeto.tags.map(tag => (
+                <span key={tag} className="tag is-info">
+                  {tag}
+                </span>
+              ))}
+            </div>
+            <a href={projeto.url} className="button is-link is-outlined">
+              Ver Projeto
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <Layout>
       <section className="section is-size-4-desktop is-size-5-touch">
-        <h1 className="title has-text-centered">Projects</h1>
+        <h1 className="title has-text-centered">Projetos</h1>
 
         <div className="columns is-multiline">
-          {projects.map(project => (
-            <div key={project.id} className="column is-one-third">
-              <div className="card">
-                <div className="card-content">
-                  <h3 className="title is-4">{project.name}</h3>
-                  <p className="subtitle is-6">{project.description}</p>
-                  <div className="tags">
-                    {project.tags.map(tag => (
-                      <span key={tag} className="tag is-info">
-                        {tag}
-                      </span>
-                    ))}
-                  </div>
-                  <a href={project.url} className="button is-link is-outlined">
-                    Ver Projeto
-                  </a>
-                </div>
-              </div>
-            </div>
-          ))}
+          {projects.map(projectRender)}
         </div>
         <p className="has-text-centered">
-          {'Some of my projects, see more on my '}
+          {'Veja mais dos meus projetos no meu '}
           <a href="http://github.com/carloskvasir">GitHub</a>
         </p>
-
       </section>
     </Layout>
   );
